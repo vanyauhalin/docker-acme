db0124af19cbe35d2fef8c7fd3f1a895d345c25e
vanyauhalin <vanyauhalin@gmail.com>
2024-05-22 21:40:20 +0400

    list all env variables in the entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,17 @@
 
 set -ue
 
+echo \
+	"$HC_URL" \
+	"$LE_CONFIG_DIR" \
+	"$LE_CRON" \
+	"$LE_DOMAINS" \
+	"$LE_EMAIL" \
+	"$LE_LOGS_DIR" \
+	"$LE_WEBROOT_DIR" \
+	"$LE_WORK_DIR" \
+	> /dev/null
+
 ./le.sh options
 ./le.sh dirs
 ./le.sh self
